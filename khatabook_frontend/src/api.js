295a6f483b8014df3891784931636d1af3c1c325
KrishSavaliya2210 <krishsavaliya2210@gmail.com>
2025-08-26 12:31:50 +0530

    chatgdb

--- khatabook_frontend/src/api.js
+++ khatabook_frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE ="http://127.0.0.1:8000/api/user";
+const API_BASE = process.env.REACT_APP_API_URL;
 
 // Create an axios instance
 const api = axios.create({
